cf12b1d7cc991a11b9b894a41dcac4f9b7665797
Ernest Ho <ernest.heh@gmail.com>
2023-05-30 20:54:08 +0800

    06 leapYear solution 2 1-line

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,14 +1,18 @@
 const leapYears = function (year) {
-  if (year % 100 === 0 && year % 400 === 0) {
-    return true;
-  } else if (year % 100 === 0 && year % 400 !== 0) {
-    return false;
-  } else if (year % 4 === 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// const leapYears = function (year) {
+//   if (year % 100 === 0 && year % 400 === 0) {
+//     return true;
+//   } else if (year % 100 === 0 && year % 400 !== 0) {
+//     return false;
+//   } else if (year % 4 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
