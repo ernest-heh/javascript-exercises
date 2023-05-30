211832efd45c113f38b95b7e14ba51015032c4d9
Ernest Ho <ernest.heh@gmail.com>
2023-05-30 20:28:47 +0800

    04 removeFromArray solution 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  const result = [];
 
+  arr.forEach((item) => {
+    if (!args.includes(item)) {
+      result.push(item);
+    }
+  });
+
+  return result;
 };
 
 // Do not edit below this line
