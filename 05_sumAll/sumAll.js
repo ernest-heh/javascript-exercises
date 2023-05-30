3528909a4603c96f4e1e52a52b2591f44e57b319
Ernest Ho <ernest.heh@gmail.com>
2023-05-30 20:43:14 +0800

    05 sumAll solution 2

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,14 @@
 const sumAll = function (start, end) {
-  let arr = [start, end].sort();
-
   if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
   if (start < 0 || end < 0) return "ERROR";
+  if (start > end) {
+    const temp = start;
+    start = end;
+    end = temp;
+  }
 
   let sum = 0;
-  for (let i = arr[0]; i <= arr[1]; i++) {
+  for (let i = start; i <= end; i++) {
     sum += i;
   }
   return sum;
@@ -13,3 +16,16 @@ const sumAll = function (start, end) {
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// const sumAll = function (start, end) {
+//   let arr = [start, end].sort();
+
+//   if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+//   if (start < 0 || end < 0) return "ERROR";
+
+//   let sum = 0;
+//   for (let i = arr[0]; i <= arr[1]; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// };
