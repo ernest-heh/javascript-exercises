aacae1b8e531cccf66d29386391b4ad0c19ca598
Ernest Ho <ernest.heh@gmail.com>
2023-05-30 20:15:03 +0800

    03 reverseString solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let split = string.split("");
+  let result = "";
+  for (let char of split) {
+    result = char + result;
+  }
+  return result;
 };
 
 // Do not edit below this line
