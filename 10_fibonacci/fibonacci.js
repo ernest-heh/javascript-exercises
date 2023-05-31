4eb04e992ab800893d0e74b2faac9a6dc8bbe2a2
Ernest Ho <ernest.heh@gmail.com>
2023-05-31 23:03:44 +0800

    10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
+  let prev = 0;
+  let curr = 1;
+  for (let i = 1; i < num; i++) {
+    const temp = curr;
+    curr = prev + curr;
+    prev = temp;
+  }
+  return curr;
 };
 
 // Do not edit below this line
