0a31d1fbdad0f3337487d2e842ea82a97a071d2d
Ernest Ho <ernest.heh@gmail.com>
2023-05-31 22:45:38 +0800

    08 calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -2,15 +2,28 @@ const add = function (a, b) {
   return a + b;
 };
 
-const subtract = function () {};
+const subtract = function (a, b) {
+  return a - b;
+};
 
-const sum = function () {};
+const sum = function (arr) {
+  return arr.reduce((total, current) => total + current, 0);
+};
 
-const multiply = function () {};
+const multiply = function (arr) {
+  return arr.length ? arr.reduce((acc, curr) => acc * curr) : 0;
+};
 
-const power = function () {};
+const power = function (a, b) {
+  return Math.pow(a, b);
+};
 
-const factorial = function () {};
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+};
 
 // Do not edit below this line
 module.exports = {
